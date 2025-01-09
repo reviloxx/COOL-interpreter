7b0647a7c78ff46e26d0143bfeadb876c18df77d
Steinwandter Oliver <oliver.steinwandter@oebb.at>
2025-01-09 07:59:44 +0100

    Adds test program

---
+++ CoolGrammarListener.cs
@@ -0,0 +1,267 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+
+namespace Cool;
+
+public class CoolGrammarListener : CoolGrammarBaseListener
+{
+    public override void EnterArithmetic([NotNull] CoolGrammarParser.ArithmeticContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterAssignment([NotNull] CoolGrammarParser.AssignmentContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterBlock([NotNull] CoolGrammarParser.BlockContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterBoolean([NotNull] CoolGrammarParser.BooleanContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterBoolNot([NotNull] CoolGrammarParser.BoolNotContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterCase([NotNull] CoolGrammarParser.CaseContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterClassDefine([NotNull] CoolGrammarParser.ClassDefineContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterComparisson([NotNull] CoolGrammarParser.ComparissonContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterDispatchExplicit([NotNull] CoolGrammarParser.DispatchExplicitContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterDispatchImplicit([NotNull] CoolGrammarParser.DispatchImplicitContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterEveryRule(ParserRuleContext ctx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterFeature([NotNull] CoolGrammarParser.FeatureContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterFormal([NotNull] CoolGrammarParser.FormalContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterId([NotNull] CoolGrammarParser.IdContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterIf([NotNull] CoolGrammarParser.IfContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterInt([NotNull] CoolGrammarParser.IntContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterIsvoid([NotNull] CoolGrammarParser.IsvoidContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterLetIn([NotNull] CoolGrammarParser.LetInContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterMethod([NotNull] CoolGrammarParser.MethodContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterNegative([NotNull] CoolGrammarParser.NegativeContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterNew([NotNull] CoolGrammarParser.NewContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterParentheses([NotNull] CoolGrammarParser.ParenthesesContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterProgram([NotNull] CoolGrammarParser.ProgramContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterProperty([NotNull] CoolGrammarParser.PropertyContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterString([NotNull] CoolGrammarParser.StringContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void EnterWhile([NotNull] CoolGrammarParser.WhileContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitArithmetic([NotNull] CoolGrammarParser.ArithmeticContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitAssignment([NotNull] CoolGrammarParser.AssignmentContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitBlock([NotNull] CoolGrammarParser.BlockContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitBoolean([NotNull] CoolGrammarParser.BooleanContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitBoolNot([NotNull] CoolGrammarParser.BoolNotContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitCase([NotNull] CoolGrammarParser.CaseContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitClassDefine([NotNull] CoolGrammarParser.ClassDefineContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitComparisson([NotNull] CoolGrammarParser.ComparissonContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitDispatchExplicit([NotNull] CoolGrammarParser.DispatchExplicitContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitDispatchImplicit([NotNull] CoolGrammarParser.DispatchImplicitContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitEveryRule(ParserRuleContext ctx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitFeature([NotNull] CoolGrammarParser.FeatureContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitFormal([NotNull] CoolGrammarParser.FormalContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitId([NotNull] CoolGrammarParser.IdContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitIf([NotNull] CoolGrammarParser.IfContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitInt([NotNull] CoolGrammarParser.IntContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitIsvoid([NotNull] CoolGrammarParser.IsvoidContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitLetIn([NotNull] CoolGrammarParser.LetInContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitMethod([NotNull] CoolGrammarParser.MethodContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitNegative([NotNull] CoolGrammarParser.NegativeContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitNew([NotNull] CoolGrammarParser.NewContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitParentheses([NotNull] CoolGrammarParser.ParenthesesContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitProgram([NotNull] CoolGrammarParser.ProgramContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitProperty([NotNull] CoolGrammarParser.PropertyContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitString([NotNull] CoolGrammarParser.StringContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExitWhile([NotNull] CoolGrammarParser.WhileContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime.Tree;
+using Antlr4.Runtime;
+using Cool;
+
+var inputFile = "bool.cl";
+var istream = new StreamReader(inputFile);
+
+AntlrInputStream input = new(istream);
+CoolGrammarLexer lexer = new(input);
+CommonTokenStream tokens = new(lexer);
+CoolGrammarParser parser = new(tokens);
+var context = parser.program();
+
+Console.WriteLine(context.ToStringTree());
+
+ParseTreeWalker walker = new();
+CoolGrammarListener listener = new();
+walker.Walk(listener, context);
